6f9c3cd386f0cd69b0e7907f78fee87a4658d947
Claudio Raverta <claudioraverta@hotmail.com>
2021-07-15 17:40:38 -0300

    minor change

--- MobileEvents.js
+++ MobileEvents.js
@@ -3,7 +3,7 @@
 function makeRequest(jsonElements, tipo) {
   var http = new XMLHttpRequest();
   console.log(tipo)
-  var url = "http://61b8317ebda9.ngrok.io/event/";
+  var url = "https://mobilelogger.claudioraverta.com/event/";
   /*var email = document.getElementById('email');
 var password = document.getElementById('pass');"*/
   console.log(url);
