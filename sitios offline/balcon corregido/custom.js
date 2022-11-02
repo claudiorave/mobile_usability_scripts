bd8aa660fa3c5bf144f845b6a248e894454c827b
Claudio Raverta <claudioraverta@hotmail.com>
2022-11-02 09:21:57 -0300

    avances en balcon plantas

--- sitios offline/balcon corregido/custom.js
+++ sitios offline/balcon corregido/custom.js
@@ -1,5 +1,38 @@
+const openTarea3 = function(){
+    sessionStorage.setItem("tarea", 3);
+    $('#searchform').submit(function() {
+        $('#searchform').submit(function() {
+            if(event.target.searchInput.value.toLowerCase() === "plantas"){
+                $("#tarea3").modal("show");
+                endSession();
+        }})
+  });
+  }
 const tarea2 = ()=>{
     event.preventDefault();
-    $("#tarea2").modal("show");
 
+    $("#tarea2").modal("show");
+}
+
+const buscador = ()=>{
+    event.preventDefault();
+    if(event.target.searchInput.value.toLowerCase() === "plantas"){
+        $("#tarea3").modal("show");
+        endSession();}
+}
+const checkMail = ()=>{
+    event.preventDefault();
+    $("#tarea4").modal("show");
+}
+
+const closeMenu = ()=>{
+$("#main-header--mobile-nav").css("display", "none");
+}
+
+const closeSearch = ()=>{
+    $("#search-form3").css("display", "none");
+    }
+const openSearch = ()=>{
+    event.preventDefault();
+    $("#search-form3").css("display") == "none" ? $("#search-form3").css("display", "block") : $("#search-form3").css("display", "none");
 }
