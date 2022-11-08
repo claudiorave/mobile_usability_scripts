b1774034742ef82077591b25165708718ad045f7
Claudio Raverta <claudioraverta@hotmail.com>
2022-11-08 19:09:48 -0300

    ajustes finales en balcon corregido

--- sitios offline/balcon corregido/custom.js
+++ sitios offline/balcon corregido/custom.js
@@ -1,17 +1,37 @@
-const openTarea3 = function(){
+$('a').click(function(ev) { ev.preventDefault(); ev.stopPropagation(); return false; });
+
+const openTarea2 = ()=>{
     sessionStorage.setItem("tarea", 3);
-    $('#searchform').submit(function() {
-        $('#searchform').submit(function() {
-            if(event.target.searchInput.value.toLowerCase() === "plantas"){
-                $("#tarea3").modal("show");
-                endSession();
-        }})
-  });
-  }
+    closeMenu();
+        $('#tareaSearch').submit(buscador);
+    } 
+const openTarea3 = ()=>{
+    closeSearch();
+    $('#formMail').submit(checkMail);
+
+}
+
+const openTarea4 = ()=>{
+    $(".menorPrecio").click(tareaFin);
+
+
+}
+
 const tarea2 = ()=>{
     event.preventDefault();
 
     $("#tarea2").modal("show");
+}
+const tarea1 = ()=>{
+    $("#tareaMenu").click(tarea2);
+}
+
+const tareaFin = ()=>{
+    event.preventDefault();
+    console.log("TAREFA");
+
+    $("#tareaFin").modal("show");
+    return true;
 }
 
 const buscador = ()=>{
@@ -22,7 +42,12 @@ const buscador = ()=>{
 }
 const checkMail = ()=>{
     event.preventDefault();
-    $("#tarea4").modal("show");
+    console.log(event.target.contactEmail.value);
+    if(event.target.contactEmail.value.toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+    $("#tarea4").modal("show");}
 }
 
 const closeMenu = ()=>{
@@ -30,9 +55,12 @@ $("#main-header--mobile-nav").css("display", "none");
 }
 
 const closeSearch = ()=>{
-    $("#search-form3").css("display", "none");
+    $("#tareaSearch").css("display", "none");
     }
+
+
 const openSearch = ()=>{
     event.preventDefault();
-    $("#search-form3").css("display") == "none" ? $("#search-form3").css("display", "block") : $("#search-form3").css("display", "none");
+    $("#tareaSearch").css("display") == "none" ? $("#tareaSearch").css("display", "block") : $("#tareaSearch").css("display", "none");
 }
+
