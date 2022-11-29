8f975cabd44db7396b26b0a949f9cfab92fcc26b
Claudio Raverta <claudioraverta@hotmail.com>
2022-11-29 18:22:06 -0300

    reparado header en kabytes

--- sitios offline/kabytes/custom.js
+++ sitios offline/kabytes/custom.js
@@ -1,111 +1,93 @@
-var clicks = 0;
-const clickSender = (event, cFunction) => {
-  console.log("CLICK CORRECTO");
-  removeDotItemPrevious();
-  removeStyleElementsInRadioPrevious();
+$('a').click(function(ev) { ev.preventDefault(); ev.stopPropagation(); return false; });
 
-  // let todos = document.getElementsByTagName("*");
-  let htmlElements = createXPathFromElement(event.target);
-  console.log(htmlElements);
-  let current_datetime = new Date();
-  let formatted_date = current_datetime.getFullYear().toString().substr(-2) + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
-  makeRequest(
-    JSON.stringify({
-      type: "click",
-      x: event.clientX,
-      y: event.clientY,
-      elements:[{xpath: htmlElements}] ,
-      timestamp:  new Date(),
-      session: sessionStorage.token,
-      sitio: sessionStorage.sitio,
-      tarea:sessionStorage.tarea
-    }),
-    cFunction
-  );
+const openTarea2 = ()=>{
+    sessionStorage.setItem("tarea", 3);
+    closeMenu();
+        $('#tareaSearch').submit(buscador);
+    } 
+const openTarea3 = ()=>{
+    closeSearch();
+    $('#formMail').submit(checkMail);
 
-  //asignarle a los elemetos un style
-  //addStyleElementsInRadio( htmlElements );
-  writeDotItem();
-};
-const clickReact = function(event)
-{   
-    clickSender(event);
+}
+
+const openTarea4 = ()=>{
+    $(".menorPrecio").click(tareaFin);
+
+
+}
+
+const tarea2 = ()=>{
     event.preventDefault();
-    clicks = clicks + 1;
-    console.log(clicks);
-    var target = event.target;
-    $(target).parent().parent().hide();
-    if(clicks == 3){
-      $("#tarea2").modal("show");
-    }  
-  }
-  const tarea2Helper = function(event){
-    clickSender(event);
+
+    $("#tarea2").modal("show");
+}
+const tarea1 = ()=>{
+    $("#tareaMenu").click(tarea2);
+}
+
+const tareaFin = ()=>{
     event.preventDefault();
-    $("#tarea3").modal("show");
+    console.log("TAREFA");
 
-  }
-  const tarea2 = function()
-  {   
-    sessionStorage.setItem("tarea", 2);
+    $("#tareaFin").modal("show");
+    return true;
+}
 
-$("#usabilidadSpan").click(tarea2Helper);
+const buscador = ()=>{
+    event.preventDefault();
+    if(event.target.searchInput.value.toLowerCase() === "plantas"){
+        $("#tarea3").modal("show");
+        endSession();}
+}
+const checkMail = ()=>{
+    event.preventDefault();
+    console.log(event.target.contactEmail.value);
+    if(event.target.contactEmail.value.toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+    $("#tarea4").modal("show");}
+}
 
+const closeMenu = ()=>{
+$("#main-header--mobile-nav").css("display", "none");
+}
+
+const closeSearch = ()=>{
+    $("#tareaSearch").css("display", "none");
     }
-    const tarea1 = ()=>{
-      sessionStorage.setItem("tarea", 1);
-      $('.sgpb-popup-close-button-2').on('touchstart mousedown click', clickReact);
-  
-    }
-    const endSession= function(){
-      var http = new XMLHttpRequest();
-      var url = "https://mobilelogger.claudioraverta.com/session/"+sessionStorage.token+"/";
-      /*var email = document.getElementById('email');
-    var password = document.getElementById('pass');"*/
-      http.open("PATCH", url, true);
-    
-      http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-          //aqui obtienes la respuesta de tu peticion
-        }
-      };
-      jsonElements = JSON.stringify({
-        active: false
-      })
-      http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      console.log(jsonElements);
-      http.send(jsonElements);
-    }
-    const tarea3 = function(event)
+
+
+const openSearch = ()=>{
+    event.preventDefault();
+    $("#tareaSearch").css("display") == "none" ? $("#tareaSearch").css("display", "block") : $("#tareaSearch").css("display", "none");
+}
+
+const openMenu = ()=>{
+  $("#menu").addClass("mm-current mm-opened");
+}
+
+const openSearchDiv = () =>{
+    $("#buscador").css("display") == "none" && $("#buscador").css("display", "block");    
+}
+
+$(document).mouseup(function(e) 
+{
+    var container = $("#menu");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
     {
-      clickSender(event, endSession);   
-  event.preventDefault();
- 
-  if(event.target.searchInput.value.toLowerCase() === "fin"){
-  $("#tareaFin").modal("show");
-  endSession();
-  }
-  
-      }
+        container.removeClass("mm-current mm-opened");
+    }
 
-      const openTarea3 = function(){
-        sessionStorage.setItem("tarea", 3);
-        $('#searchform').submit(function() {
-          return tarea3(event);
-      });
-      }
+    var searchDiv = $("#buscador")
+    var searchForm = $("#searchform")
 
-      
-  $('a').click(function(ev) { ev.preventDefault(); ev.stopPropagation(); return false; });
-  $('.sg-popup-builder-content').click(function(ev) { ev.preventDefault(); ev.stopPropagation(); return false; });
-  $('.popup').click(function(ev) { 
-    ev.preventDefault(); ev.stopPropagation(); return false; });
-    $('#searchform').submit(function(event) {
-    
-      clickSender(event);
-      event.preventDefault();
-  });
+    if (!searchForm.is(e.target) && searchForm.has(e.target).length === 0) 
+    {
+        searchDiv.css("display", "none");
+    }
 
-
-
-  
+});
